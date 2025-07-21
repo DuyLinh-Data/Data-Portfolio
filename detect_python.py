2865b230b0c248dcea9be944bc93cf83c2c7cff5
Antoni Nguyen <linhnguyen.asia@gmail.com>
2025-07-21 15:56:57 +0700

    Delete detect_python.py

--- detect_python.py
+++
@@ -1,8 +0,0 @@
-# Dummy file to help GitHub recognize this repo as Python
-# This file does not contain any core logic
-
-def say_hello():
-    print("Hello from Python!")
-
-say_hello()
-add dummy python file
